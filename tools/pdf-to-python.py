8922e19c5099d85b7fca820da80082cf904d134c
Ruslan <20125922+rkrenzler@users.noreply.github.com>
2017-12-16 21:18:42 +0100

    add pipe size name to the table generated by pdf-to-python.py

--- tools/pdf-to-python.py
+++ tools/pdf-to-python.py
@@ -37,7 +37,10 @@ def process_line(line, schedule):
     # 0-th word contains a string value. Put it between quotation marks.
     first = words.pop(0)
     result = "'"+first+"\'"
-    # 1-word contains the scheduler value
+    # 1-th word will be added converted to astring with a quantity
+    # with inch unite. That is 1/2->'1/2"'.
+    result += ", '"+words[1]+'"\''
+    # 2-word contains the scheduler value
     result += ", %d"%schedule
     for word in words:
         result+=", "+process_value(word)
